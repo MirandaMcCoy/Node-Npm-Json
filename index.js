da4a6fe4241d7b66d1ddac95983593128f36d381
MCCOY <mirandamccoy5@gmail.com>
2017-11-01 09:45:28 -0500

    used read-json-file and lodash to display data.json info to console

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-var readJson = require('read-package-json');
+var readJson = require('read-json-file');
+var lodash = require('lodash');
 
+readJson('data.json', function(error, data){
+    if (error){
+        throw error;
+    }
+    console.log(lodash.map(data.items, 'fname'));
+    console.log(lodash.map(data.items, 'lname'));
+    console.log(lodash.map(data.items, 'age'));
+});
